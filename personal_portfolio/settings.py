1836f61ec159aa33370167a38aeb1d72dfc9d01b
KuraiYoru <artsermark@mail.ru>
2022-06-14 14:21:45 +0300

    new local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6j9)wl(a$1zq1-_+w9q3#0%pvi)g^vkc^0ruzy56@f(93lym9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mark123.pythonanywhere.com']
 
 
 
@@ -130,3 +130,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("No loacal  file")
